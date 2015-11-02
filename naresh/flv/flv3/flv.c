89c8c3c62026c096a2c2d2c266690f09b87d4ada
bhagavan <bhagavan@auranetworks.in>
2015-11-02 17:45:48 +0530

    Adding new files

---
+++ naresh/flv/flv3/flv.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+#include "flv.h"
+
+int read_flv_header(int, char *,int);
+void parse_flv_header(char *, int, struct header *);
+void dump_flv_header(struct header *);
+
+int read_flv_tag(int,char *, int);
+void parse_flv_tag(char *, int ,struct flv_tag *);
+void dump_flv_tag(struct flv_tag *);
+
+main()
+{
+	struct header fhead;
+	struct flv_tag ftag;
+
+	int fd = 0, r = 0, r1 = 0;
+	char buff[128];
+
+	fd = open("sixth.flv", O_RDONLY, 0777);
+
+	r = read_flv_header(fd, buff, 9);
+	parse_flv_header(buff, 9, &fhead);
+	dump_flv_header(&fhead);
+
+	lseek(fd, 0, SEEK_CUR);
+	r1 = read_flv_tag(fd, buff,12);
+	parse_flv_tag(buff, 12, &ftag);
+	dump_flv_tag(&ftag);
+}
