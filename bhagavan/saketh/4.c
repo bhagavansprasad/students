b10992ece58d8212b9f67837a7ae773e7e89f2a1
bhagavan <bhagavan@auranetworks.in>
2016-03-27 18:05:22 +0530

    Adding files

---
+++ bhagavan/saketh/4.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+main()
+{
+	int a=1;
+	printf("a:%d\n",a);
+
+	a = a + 1;
+	printf("a:%d\n",a);
+
+	a = a + 1;
+	printf("a:%d\n",a);
+
+	a = a + 1;
+	printf("a:%d\n",a);
+
+	a = a + 1;
+	printf("a:%d\n",a);
+}
---
+++ bhagavan/saketh/6.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+main()
+{
+	int n = 10;
+	int i = 1;
+
+	for (i = 1; i <= n; i++) 
+	{
+		printf("I am Saketh I am printing i value :%d\n", i);
+	}
+}
