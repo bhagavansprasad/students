ab8c3275e384f112d1c1e1e716e723aefef32bc0
bhagavan <bhagavan@auranetworks.in>
2016-05-14 16:26:32 +0530

    adding

---
+++ rajini/OS/assign/execfn/exe1.c
@@ -0,0 +1,12 @@
+main(argc,argv)
+ 
+int argc;
+char *argv[];
+{
+  printf("Before ecec mmy ID is %d \n",getpid());
+  printf("exec starts\n");
+  execl(argv[1],argv[2],argv[3],argv[4],(char *)0);
+  printf("this will not print\n");
+
+}
+
