3f78be43fc8e47c222a780a4bcb54e1fd2c0f5d5
bhagavan <bhagavan@stblabs.com>
2017-02-24 12:03:25 +0530

    adding some more functions

--- bhagavan/test_prog/strings/strlen.c
+++ bhagavan/test_prog/strings/strlen.c
@@ -27,15 +27,15 @@ int get_small_case_count(char *pname)
 
 //Return 0 if both are same
 //Return 1 if they are not same
-int string_compare(char *s1, char *s2)
+int astrcmp(char *s1, char *s2)
 {
 }
 
-int string_to_lower(char *s)
+int astrtolower(char *s)
 {
 }
 
-int string_to_upper(char *s)
+int astrtoupper(char *s)
 {
 }
 
@@ -60,9 +60,9 @@ int is_upper(char ch)
 main()
 {
 	char sname[100] = "Bhagavathi Tamilnadu";
-	char s1[100] = "Bh";
-	char s2[100] = "a";
-	char s3[100] = "    ";
+	char s1[100] = "How are you";
+	char s2[100] = "How are you";
+	char s3[100] = "How and are you";
 	char s4[100] = "";
 	int len = 0;
 	char ch1 = 'x'
@@ -98,7 +98,44 @@ main()
 	else
 		printf("%c is a lower\n");
 
+	// xxxxx has 2 capital letters and 18 small letters
+	printf("%s : capital count %d and %d small letters", sname, get_capital_case_count(sname), get_small_case_count(sname));
 
+	printf("Before astrtoupper sname :%s", sname); //Bhagavathi
+	astrtoupper(sname);
+	printf("After  astrtoupper sname :%s", sname); //BHAGAVATHI
+
+	printf("Before astrtosentencecase sname :%s", sname); //BHAGAVATHI //bhagavathi //bHaGaVatHi
+	astrtosentencecase(sname);
+	printf("After  astrtosentencecase sname :%s", sname); //Bhagavathi
+
+	printf("Before astrtolower sname :%s", sname); //Bhagavathi
+	astrtoupper(sname);
+	printf("After  astrtolower sname :%s", sname); //bhagavathi
+
+	retval = astrcmp(s1, s2)  // SAME
+	if (retval == 0)
+		printf("%s and %s are SAME\n", s1, s2);
+	else
+		printf("%s and %s are NOT SAME\n", s1, s2);
+
+	retval = astrcmp(s2, s1)  // SAME
+	if (retval == 0)
+		printf("%s and %s are SAME\n", s2, s1);
+	else
+		printf("%s and %s are NOT SAME\n", s2, s1);
+
+	retval = astrcmp(s2, s3)  // NOT SAME
+	if (retval == 0)
+		printf("%s and %s are SAME\n", s2, s3);
+	else
+		printf("%s and %s are NOT SAME\n", s2, s3);
+
+	retval = astrcmp(s1, s3)  // NOT SAME
+	if (retval == 0)
+		printf("%s and %s are SAME\n", s1, s3);
+	else
+		printf("%s and %s are NOT SAME\n", s1, s3);
 }
 
 
