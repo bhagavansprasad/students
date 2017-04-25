7dc9f6675711ef54d036377ddf401db320a701b7
bhagavan <bhagavan@stblabs.com>
2017-04-25 11:19:32 +0530

    adding more functionality

--- ganga/pointers/fun-ptrs/6-fun-ptr.c
+++ ganga/pointers/fun-ptrs/6-fun-ptr.c
@@ -64,6 +64,39 @@ struct fun_data data_list[4] = {
 	{ "div", division }
 };
 
+char test_strs[10][100] = {
+	"add 10 20",
+	"    add      10     20     ",
+	"    Add      10     20     ",
+	"    ADD      10     20     ",
+	"    mul      10     20     ",
+	"    MUL      10     20     ",
+	"    MUL      10     20   30   ",
+	"    MUL         20     ",
+	"    MUlL    ",
+};
+
+int parse_arguments(char *src, char *pcmd, int *pa, int *pb)
+{
+
+}
+
+int perform_operation(char *pcmd, int a, int b)
+{
+	int result = 0;
+
+	for (i = 0; i <= 3; i++)
+	{
+		sope = strcmp(data_list[i].operation, cmd);
+		if (sope == 0)
+		{
+			result = data_list[i].fp(a, b);
+		}
+	}
+	return result;
+
+}
+
 main()
 {
 	int a = 10;
@@ -74,13 +107,10 @@ main()
 
 	printf("%d. %s, %s\n", __LINE__, __FUNCTION__, __FILE__);
 
-	for (i = 0; i <= 3; i++)
+	for (i = 0; i < sizeof(test_strs)/sizeof(test_str[i]); i++)
 	{
-		sope = strcmp(data_list[i].operation, cmd);
-		if (sope == 0)
-		{
-			result = data_list[i].fp(a, b);
-			printf("%d and %d = %d\n", a, b, result);
-		}
+		parse_arguments(test_strs[i], cmd, &a, &b);
+		result = perform_operation(cmd, a, b);
+		printf("%d and %d = %d\n", a, b, result);
 	}
 }
