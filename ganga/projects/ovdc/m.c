f51d862887f893a036bf5caf34e954f34c9ed79d
bhagavan <bhagavan@auranetworks.in>
2017-06-06 14:14:02 +0530

    adding m.c

---
+++ ganga/projects/ovdc/m.c
@@ -0,0 +1,54 @@
+char data[1024] = "2061 (a.out) R 1977 2061 1977 34819 2061 4218880 186 0 0 0 116758 26 0 0 20 0 1 0 22053 4259840 88 18446744073709551615 4194304 4196460 140731199456592 140731199455848 4195916 0 0 0 0 0 0 0 17 1 0 0 0 0 0 6295080 6295608 27226112 140731199464412 140731199464422 140731199464422 140731199467504 0";
+
+
+int get_giffs(char *pbuff)
+{
+	for( i = 0; buff[i] != '\0'; i++)
+	{
+	}
+}
+#if 0
+		if( buff[i] == ' ' && buff[i +1] != ' ' && buff[i + 1] != '\0')
+		{
+			count = count + 1;
+			//printf("count : %d\t", count);
+		}
+
+		if( count == 13)
+		{
+			for( ; buff[i] = ' ' && buff[i] != '\0'; i++);
+
+			for ( ; buff[i] != ' ' && buff[i] != '\0'; i++, j++)
+			{
+				val13[j] = buff[i];
+				val13[j] = '\0';
+			}
+			v13 = (int)atoi(val13);
+			printf(" 13 word is %d\n", v13 );
+			//printf(" i --> %d %c\n", i, buff[i]);
+
+			for( ; buff[i] == ' ' && buff[i] != '\0'; i++);
+			//printf(" i --> %d %c\n", i, buff[i]);
+			//printf("entering\n");
+
+			for (j = 0 ; buff[i] != ' ' && buff[i] != '\0'; i++, j++)
+			{
+				printf(" i --> %d %c-->\n", i, buff[i]);
+				printf("entering\n");
+				printf(" 14 word is %s:\n", val14);
+			}
+			val14[j] = buff[i];
+			val14[j] = '\0';
+
+			for( ; buff[i] = ' ' && buff[i] != '\0'; i++);
+
+		}
+	}
+
+}
+#endif
+
+main()
+{
+	get_giffs(data);
+}
