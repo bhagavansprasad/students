b009916012121017a7106b5ad579098c029639c6
bhagavan <bhagavan@stblabs.com>
2017-07-04 13:04:30 +0530

    fixing issues

--- likhitha/projects/ovdc/main.c
+++ likhitha/projects/ovdc/main.c
@@ -1,82 +1,39 @@
 #include <stdio.h>
-#include<fcntl.h>
-#include"string.h"
-#include"ovdc.h"
-int get_giffs_by_pid(int);
-struct pid_giffs_db giffs_data [10] = {0, 0};
-struct ovdc_data data;
+#include <fcntl.h>
+#include "string.h"
+#include "stdlib.h"
+#include "ovdc.h"
+
+struct ovdc_data data[100];
+
 int pid_count = 0;
 int i=0 ;
 
 int store_n_get_cpu_occ(int pid, int giffs)
 {
-	int prev_giffs=0;
 	int cpu_occ= 0;
 	int i,pid_count=0;
+
 	for(i = 0; i < pid_count; i++)
 	{
-		if (giffs_data[i].pid == pid)
+		if (data[i].pid == pid)
 		{
-			//TODO:
-			//take the diff between old and new value
-			//replace old giffs with current giffs
-			//return diff
-			giffs_data[i].curr_giffs = giffs;
-			cpu_occ  = giffs - prev_giffs;
-			prev_giffs = giffs;
-			//giffs_data[i].cpu_occ= cpu_occ;
+			cpu_occ  = giffs - data[i].giffs;
+			data[i].giffs = giffs;
+
 			return cpu_occ;
-
 		}
-		//TODO
-		//Its a new pid
-		//Make a new entry in db
-		//increment pid_count
-		//return cpu occpance as 0
-
-		else
-		{
-			giffs_data[i].pid = pid;
-			pid_count++;
-			//	giffs_data[i].giffs = giffs;
-			//	giffs_data[i].cpu_occ = cpu_occ;
-			return 0;
-		}
-
 	}
-
-}
-
-int get_giffs_by_pid(int pid)
-{
-	int fd=-1 , words = 0, retval=0 ,len=2048 , giffs_count;
-	char buff[4*1024];
-	char temp[100] = "" ;
-
-	sprintf(temp, "/proc/%d/stat", pid);
-
-	fd = open(temp, O_RDONLY);
-	if( fd < 0)
-	{
-		printf( "open failure\n");
-		return ;
-	}
-
-	retval = read(fd, buff, len);
-	retval[buff] = '\0';
-	close(fd);
-
-	printf("%s", buff);
-	//printf("%s\n", pbuff); 
-
-	//TODO:
-	giffs_count = get_cpu_giffs_sum(buff);
-	return giffs_count;
+	data[i].pid = pid;
+	data[i].giffs = giffs;
+	pid_count++;
+	return 0;
 }
 
 int get_cpu_giffs_sum(char *buff)
 {
-	int words=0 , sum=0 , temp[100] , i=0 , value=0 , j=0;
+	int words=0 , sum=0 , i=0, value=0, j=0;
+	char temp[100]= " ";
 	for( ; words != 13 && buff[i] != '\0'; i++) //getting 14th word
 	{
 		if (buff[i] == ' ')
@@ -93,7 +50,7 @@ int get_cpu_giffs_sum(char *buff)
 			temp[j] = buff[i];
 		temp[j] = '\0';
 		i++;
-		value =(int) atoi(temp); //converting ascii to integer
+		value = (int)atoi(temp); //converting ascii to integer
 		printf("value :%d\n", value);
 		sum = sum + value;
 		//	i++;	
@@ -103,6 +60,32 @@ int get_cpu_giffs_sum(char *buff)
 	//printf("sum=%d\n",sum);
 
 	return sum;
+}
+
+int get_giffs_by_pid(int pid)
+{
+	int fd=-1 , words = 0, retval=0 ,len=2048 , giffs_count;
+	char buff[4*1024];
+	char temp[100] = "" ;
+
+	sprintf(temp, "/proc/%d/stat", pid);
+
+	fd = open(temp, O_RDONLY);
+	if(fd < 0)
+	{
+		printf( "open failure\n");
+		return -1;
+	}
+
+	retval = read(fd, buff, len);
+	retval[buff] = '\0';
+	close(fd);
+
+	printf("%s", buff);
+	//printf("%s\n", pbuff); 
+
+	giffs_count = get_cpu_giffs_sum(buff);
+	return giffs_count;
 }
 
 int send_giffs(int pid, int giffs)
@@ -120,29 +103,34 @@ int send_giffs(int pid, int giffs)
 	} 
 }
 
+int get_pids_from_args(int *pids, int argc, char *argv[])
+{
+	for(i = 1; i < argc; i++)
+		pids[i-1] = atoi(argv[i]);
+
+	return argc-1;
+}
+
 main(int argc, char *argv[])
 {
-	int giffs=0 ,j, i , proc_count = 2 ,retval , pid[5] ;
-	int cpu_occ=-1 ,  prev_giffs=0;
-	struct ovdc_data data;
-	//TODO: Store pids that are pass as command line arguments
-	//int pid[100]= {  };
+	int giffs=0 ,j, i , proc_count, retval;
+	int cpu_occ=-1, prev_giffs=0;
+	int pids[100];
 
-	for( j = 0; j< 3; j++)
+	proc_count = get_pids_from_args(pids, argc, argv);
+	
+	for (i = 0; i < proc_count; i++)
 	{
-		for(i=1; i <= proc_count; i++)
-		{
-			pid[i] = atoi(argv[i]);
-			giffs = get_giffs_by_pid(pid[i]);
-			if(giffs!=-1)
-				send_giffs(pid[i],giffs);
-		}
-		//data.pid=-1;
-		//data.giffs=-1;
-		//retval = write(&data, sizeof(data));
+		printf ("-->%d. %d\n", i+1, pids[i]);
 	}
-	cpu_occ = store_n_get_cpu_occ(pid[i], giffs);
-	printf("\n cpu occupancy %d\n",cpu_occ);
+
+	for (i = 0; i < proc_count; i++)
+	{
+		giffs = get_giffs_by_pid(pids[i]);
+		cpu_occ = store_n_get_cpu_occ(pids[i], giffs);
+	}
+
+	printf("-->cpu occupancy %d\n", cpu_occ);
 	sleep(5);
 }
 
--- likhitha/projects/ovdc/ovdc.h
+++ likhitha/projects/ovdc/ovdc.h
@@ -1,11 +1,5 @@
 struct ovdc_data {
-int pid;
-int giffs;
-};
-
-struct pid_giffs_db
-{
 	int pid;
-	int curr_giffs;
+	int giffs;
 };
 
