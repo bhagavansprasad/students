0688eed656e8d96f591eb28403d3b6f0773bf77a
bhagavan <bhagavan@auranetworks.in>
2017-07-07 09:38:22 +0530

    adding

--- ganga/projects/ovdc/new_ovdc/ovc.c
+++ ganga/projects/ovdc/new_ovdc/ovc.c
@@ -1,13 +1,49 @@
 #include <stdio.h>
+#include "defs.h"
+
+struct ovdc_db data[100]= {0};
+int pid_count = 0;
+int store_n_get_cpu_occ(int pid, int giffs)
+{
+	int i, cpu_occ = 0;
+
+	for(i = 0; i < pid_count; i++)
+	{
+		if (data[i].pid == pid)
+		{
+			cpu_occ  = giffs - data[i].giffs;
+			data[i].giffs = giffs;
+
+			return cpu_occ;
+		}
+	}
+	data[i].pid = pid;
+	data[i].giffs = giffs;
+	pid_count++;
+
+	return 0;
+}
 
 int ovc(int rfd)
 {
-	int pid, giffs;
-	for(; ;)
+	int i, pid, giffs, cpu_occ;
+	int pids[100] = {0};
+	struct ovdc_db data;
+
+	for( ; ; )
 	{
-		read(pid, giffs);
-		store_index_db();
-		calc_cpu_occ();
+		//TODO:
+		//read structure with pid and giffs from pipe
+		
+		read(rfd, &data, sizeof(data));
+		cpu_occ = store_n_get_cpu_occ(data.pid, data.giffs);
+		printf("%d:%d\n", pids[i], cpu_occ);
+
+#if 0
+		read(pid,giffs);
+		store_in_db();
+		calculate_cpu_occ();
 		action();
+#endif
 	}
 }
--- ganga/projects/ovdc/new_ovdc/ovd.c
+++ ganga/projects/ovdc/new_ovdc/ovd.c
@@ -1,15 +1,100 @@
 #include <stdio.h>
+#include "defs.h"
+#include "fcntl.h"
 
-int ovd(int wfd)
+int get_cpu_giffs_sum(char *buff)
+{
+	int words = 0, sum = 0, i = 0, value = 0, j = 0;
+	char temp[100] = " ";
+	for( ; words != 13 && buff[i] != '\0'; i++) //getting 14th word
+	{
+		if (buff[i] == ' ')
+			words++;
+	}				
+	//printf("i     :%d\n", i); 
+	//printf("words :%d\n", words); 
+	//printf("%s\n", &pbuff[i]); 
+
+	sum = 0;
+	for(words = 0 ; words < 4; words++)
+	{
+		for( ; buff[i] != ' '; i++, j++)
+			temp[j] = buff[i];
+		temp[j] = '\0';
+		i++;
+		value = (int)atoi(temp); //converting ascii to integer
+		//printf("value :%d\n", value);
+		sum = sum + value;
+		//i++;	
+		j = 0;
+	}
+	//printf("sum=%d\n",sum);
+
+	return sum;
+}
+
+
+int get_giffs_by_pid(int pid)
+{
+	int fd = -1, retval = 0, len = 2048, giffs_count;
+	char buff[4*1024];
+	char temp[100] = "" ;
+
+	sprintf(temp, "/proc/%d/stat", pid);
+
+	fd = open(temp, O_RDONLY);
+	if(fd < 0)
+	{
+		printf( "open failure\n");
+		return -1;
+	}
+
+	retval = read(fd, buff, len);
+	retval[buff] = '\0';
+	close(fd);
+
+	//printf("%s", buff);
+	//printf("%s\n", pbuff); 
+
+	giffs_count = get_cpu_giffs_sum(buff);
+	return giffs_count;
+}
+
+int ovd(int wfd, int *pids, int proc_count)
 {
 	int pid, giffs;
-	for(; ; )
+	int i = 0;
+	struct ovdc_db data;
+
+	while(1)
+	{
+		for (i = 0; i < proc_count; i++)
+		{
+			printf("-->%d. %d\n", i+1, pids[i]);
+			giffs = get_giffs_by_pid(pids[i]);
+				
+			data.pid = pids[i];
+			data.giffs = giffs;
+
+			write(wfd, &data, sizeof(data));
+
+			//TODO:
+			//Fill the sturcture with pid and giffs
+			//write on to pipe
+
+		}
+		sleep(1);
+	}
+
+
+#if 0
+	for( ; ; )
 	{
 		open();
 		read();
-		send(pid, giffs);
-
-		sleep(2);
+		send(pid,giffs);
+		sleep();
 		return 0;
 	}
+#endif
 }
