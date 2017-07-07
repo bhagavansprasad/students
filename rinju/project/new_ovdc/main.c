5366de075629e6df182761ddc13ba54ed7eb8155
bhagavan <bhagavan@bhagavan>
2017-07-07 11:19:16 +0530

    add

--- rinju/project/new_ovdc/main.c
+++ rinju/project/new_ovdc/main.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
-main()
+
+int get_pids_from_args(int *pids, int argc, char *argv[])
 {
-	int p[2] ,retval , pid;
-	retval = pipe(p);
+	int i = 0;
+
+	for(i = 1; i < argc; i++)
+		pids[i-1] = atoi(argv[i]);
+
+	return argc-1;
+}
+
+int main(int argc, char *argv[])
+{
+	int pipe_descriptors[2];
+	int pid, retval ,i;
+	int giffs=0, proc_count ;
+	int pids[100];
+
+	proc_count = get_pids_from_args(pids, argc, argv);
+
+	for (i = 0; i < proc_count; i++)
+	{
+		printf("-->%d. %d\n", i+1, pids[i]);
+	}
+
+	retval = pipe(pipe_descritors);
 	pid = fork();
 	if(pid == 0)
 	{
-		ovd(p[0]);
+		close(pipe_descriptors[0]);
+		ovd(pipe_descriptors[1], pids, proc_count);
 	}
 	else
 	{
-		ovc(p[1]);
+		close(pipe_descriptors[1]);
+		ovc(pipe_descriptors[0]);
 	}
+
 }
+
