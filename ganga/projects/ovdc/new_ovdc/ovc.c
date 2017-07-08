00dae51264c2ecc7eec6e9099a671b99b5b9d5c4
bhagavan <bhagavan@auranetworks.in>
2017-07-08 11:36:07 +0530

    add

--- ganga/projects/ovdc/new_ovdc/ovc.c
+++ ganga/projects/ovdc/new_ovdc/ovc.c
@@ -1,54 +1,49 @@
 #include <stdio.h>
 #include "defs.h"
 
-int db[100][3];
-struct ovdc_db data[100]= {0};
 int pid_count = 0;
 
 int store_n_get_cpu_occ(int pid, int giffs)
 {
 	int i, cpu_occ = 0;
+	struct ovdc_db d[100] = {0, 0};
 
 	for(i = 0; i < pid_count; i++)
 	{
-		if (db[i][0] == pid)
+		if (d[i].pid == pid)
 		{
-			cpu_occ  = giffs - db[i][1];
-			db[i][1] = giffs;
-			
-			//cpu_occ  = giffs - data[i].giffs;
-			//data[i].giffs = giffs;
+			cpu_occ  = giffs - d[i].giffs;
+			d[i].giffs = giffs;
 
 			return cpu_occ;
 		}
 	}
-	db[i][0] = pid;
-	db[i][1] = giffs;
-	pid_count++;
+	
+	for(  ; i < 100; i++)
+	{
+		d[i].pid = pid;
+		d[i].giffs = giffs;
+		pid_count++;
 
-	return 0;
+		return 0;
+	}
 }
 
 int ovc(int rfd)
 {
 	int i, pid, giffs, cpu_occ;
 	int pids[100] = {0};
-	struct ovdc_db data;
-
-	read(rfd, &db, sizeof(db));
-	for( ; ; )
+	struct ovdc_db dta;
+	
+	while(1)
 	{
-		//TODO:
-		//read structure with pid and giffs from pipe
-		
-		cpu_occ = store_n_get_cpu_occ(db[i][0], db[i][1]);
-		printf("%d:%d\n",db[i][0], cpu_occ);
+		for( ; ; )
+		{
+			read(rfd, &dta, sizeof(dta));
+			cpu_occ = store_n_get_cpu_occ(dta.pid, dta.giffs);
+
+			if(cpu_occ > 0)
+				printf("CPU_OCC : %d\n", cpu_occ);
+		}
 	}
 }
-
-#if 0
-read(pid,giffs);
-store_in_db();
-calculate_cpu_occ();
-action();
-#endif
