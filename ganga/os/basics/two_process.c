934d5963bb35160dec9fdc875f636d238b1e4441
bhagavan <bhagavan@auranetworks.in>
2017-07-10 14:39:50 +0530

    add

---
+++ ganga/os/basics/two_process.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int c = 10;
+
+main()
+{
+	int pid = fork();
+	if(pid == 0)
+	{
+		while(1)
+		{
+			printf("--> c vaule in child is :%d\n", c);
+			printf("--> c address in child is :%p\n", &c);
+			printf("pid: %d\n", getpid());
+			printf("--> ppid: %d\n", getppid());
+			sleep(2);
+		}
+	}
+	else
+	{
+		while(1)
+		{
+			c++;
+			printf("--> c vaule in parent is :%d\n", c);
+			printf("--> c address in parent is :%p\n", &c);
+			printf("pid: %d\n", getpid());
+			printf("--> ppid: %d\n", getppid());
+			sleep(2);
+		}
+	}
+}
