01d301c1c3c5db2abd58ac087d31f3a65ee6f101
bhagavan <bhagavan@stblabs.com>
2017-07-10 15:03:04 +0530

    fixing issues

--- rinju/assignment3/process_management/test3.c
+++ rinju/assignment3/process_management/test3.c
@@ -1,33 +1,36 @@
-
 #include <stdio.h>
 #include<fcntl.h>
-int a=10;
-main()
 
+int a=10;
+
+main()
 {	
-	//FILE *fp;
 	int pid,fd;
 	char buff[100] = "hi how are you";
 	char arr[50];
 
+	fd=open("temp.txt",O_CREAT |O_RDWR);
 
-	fd=open("temp.tx",O_CREAT |O_RDWR);
 	pid=fork();
+
 	if(pid == 0)
 	{
-		
-			int retval=	write(fd,buff,100);
-			printf("i am child\n");
+		int retval=	write(fd, buff, 100);
+		printf("C: retval %d\n", retval);
+		close(fd);
 	}
 	else
-	{int retval;
+	{
+		int retval;
 
-			
-	      while( retval=read(fd,arr,100)){
+		sleep(2);
+		fd = open("temp.txt", O_RDWR);
+		retval=read(fd, arr, 100);
 
-			arr[retval]='\0';
-			printf("%s",arr);}
-			printf("i am parent\n");
+		printf("P:\n");
+		printf("P: retval %d\n", retval);
+
+		arr[retval]='\0';
+		printf("P: %s\n", arr);
 	}
-			close(fd);	
 }
