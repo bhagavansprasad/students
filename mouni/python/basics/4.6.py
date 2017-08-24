040e5132a934d226948bdfc83d13640a36ab8135
bhagavan <bhagavan@auranetworks.in>
2017-08-24 09:11:49 +0530

    fixing the bug

--- mouni/python/basics/4.6.py
+++ mouni/python/basics/4.6.py
@@ -1,9 +1,12 @@
-n=5
+n = 3
+
+a = [[0 for i in range(n)] for j in range(n)]
+
 i = 0
 j = n / 2
-a=[[0 for i in range(n)] for j in range(n)]
+
 for k in range(1,n*n + 1):
-	a[i][j]=k,
+	a[i][j] = k
 	i=i-1
 	j=j+1
 	if(k%n==0):
@@ -13,8 +16,9 @@ for k in range(1,n*n + 1):
 		j=j-n	
 	elif(i<0):
 		i=i+n
+
 for i in range(0,n):
 	for j in range(0,n):
-		print a[i][j],
-	print '\n'
+		print ("%-3d" % (a[i][j])),
+	print ''
 
