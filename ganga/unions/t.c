96f5919b1ca3eb3e2c2949f072ab962d1bbd6dd5
bhagavan <bhagavan@stblabs.com>
2017-09-06 13:40:24 +0530

    adding test program

---
+++ ganga/unions/t.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+union t
+{
+	char ar[4];
+	int a;
+};
+
+union t x = {"abcd"};
+
+main()
+{
+	int i = 0;
+
+	for (i = 0; i <=3 ; i++)
+		printf ("%p: %d\n", ((char *)&(x.a))+i, *(((char *)&(x.a))+i));
+
+	printf("\n");
+
+	for (i = 0; i <=3 ; i++)
+		printf ("%p: %d\n", &(x.ar[i]), x.ar[i]);
+}
