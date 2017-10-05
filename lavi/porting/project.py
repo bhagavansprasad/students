a6b6d6c95d169bceb183bb3e3d3291864e6206c8
bhagavan <bhagavan@auranetworks.in>
2017-10-05 15:53:35 +0530

    adding more test strings

--- lavi/porting/project.py
+++ lavi/porting/project.py
@@ -1,12 +1,28 @@
-s = '\0'
-def port_to_3x(str):
-	s = str.replace( "print" , " print (")
-	s = s  + ")" 
-	print s
-	return
+porting_strins = ["print", "print ("]
 
-port_to_3x("print'Aura Networks'")
-port_to_3x("print'VR Chambers'")
-port_to_3x("print'Kadubisanahalli'")
-port_to_3x("print'Bangalore'")
-port_to_3x("print'560103'")
+def python_port2x_to_3x(str):
+	new_string = str.replace(porting_strins[0], porting_strins[1])
+	new_string = new_string  + ")" 
+
+	return new_string
+
+test_strings = [
+	"print'Aura Networks'",
+	"print'VR Chambers'",
+	"print'Kadubisanahalli'",
+	"print'Bangalore'",
+	"print'560103'",
+	'print "560103"',
+	'print ("560103")',
+	'print    ("560103")',
+	'print                                 ("560103")',
+	'print("560103")',
+	'printing my line',
+	'Hi I am printing my documet',
+	"print '560103'",
+	"def print_all_files_recursively(path):"
+
+	]
+
+for nstr in test_strings:
+	print (python_port2x_to_3x(nstr))
