8aa0447a2206755790473101d108ff15ff98920c
bhagavan <bhagavan@auranetworks.in>
2017-10-10 16:23:24 +0530

    adding aura 2to3 program

---
+++ lavi/porting/a2to3.py
@@ -0,0 +1,31 @@
+porting_strins = ["print", "print ("]
+
+def python_port2x_to_3x(line):
+	new_string = line.replace(porting_strins[0], porting_strins[1])
+	new_string = new_string  + ")" 
+
+	return new_string
+
+test_strings = [
+	"print'Aura Networks'",
+	"print'VR Chambers'",
+	"print'Kadubisanahalli'",
+	"print'Bangalore'",
+	"print'560103'",
+	'print "560103"',
+	'print ("560103")',
+	'	print ("560103")',
+	'print xyz("560103")',
+	'print    ("560103")',
+	'print                                 ("560103")',
+	'myprint                                 ("560103")',
+	'	myprint                                 ("560103")',
+	'print("560103")',
+	'printing my line',
+	'Hi I am printing my documet',
+	"print '560103'",
+	"def print_all_files_recursively(path):"
+	]
+
+for nstr in test_strings:
+	print (python_port2x_to_3x(nstr))
