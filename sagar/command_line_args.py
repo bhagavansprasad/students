09dfe82dc64c586e11271f42979c6cd26e1106fe
bhagavan <bhagavan@auranetworks.in>
2017-10-25 11:05:03 +0530

    final

--- sagar/command_line_args.py
+++ sagar/command_line_args.py
@@ -1,7 +1,6 @@
 import sys
 
 print (sys.argv)
-#exit(1)
 print (sys.argv[0])
 print (sys.argv[1])
 print (sys.argv[2])
@@ -12,6 +11,33 @@ for args in sys.argv:
     print (args)
 
 
-ofd = open(sys.argv[1], "r")
-nfd = open(sys.argv[2], "r")
+#ofd = open(sys.argv[1], "r")
+#nfd = open(sys.argv[2], "r")
+
+
+#fd1 = open("sys.argv[1]", "rt")
+fd1= open(sys.argv[1], "rt")
+data = fd1.read().replace("\n", '')
+newdata="warning:"
+
+old_warn_count = data.count(newdata)
+fd1.close()
+
+#fd2 = open("argv[2]", "rt")
+fd2= open(sys.argv[2], "rt")
+data = fd2.read().replace("\n",'')
+newdata="warning:"
+
+new_warn_count=data.count(newdata)
+fd2.close()
+
+if(new_warn_count <= old_warn_count):
+	print("*** Build promoted ***")
+else:
+	print("*** Build NOT promoted ***")
+
+print("   Old warning count :%d" % old_warn_count)
+print("   New warning count :%d" % new_warn_count)
+
+
 
