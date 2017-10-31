bb4e4bfc91b8b31b41da0875916dc94b11366851
bhagavan <bhagavan@auranetworks.in>
2017-10-31 14:50:34 +0530

    cipher

---
+++ sagar/cipherr.py
@@ -0,0 +1,19 @@
+
+n=input()
+l=len(n)
+k=int(input())
+up=''
+for i in range(0,l):
+	if(n[i].isalpha()==True):
+		if(n[i].islower()==True):
+			up=ord(n[i])+k
+			if(up>122):
+				up=up-26	
+		if(n[i].isupper()==True):
+			up=ord(n[i])+k
+			if(up>90):
+				up=up-26
+				
+		print(chr(up),end="")
+	else:
+		print(n[i])
