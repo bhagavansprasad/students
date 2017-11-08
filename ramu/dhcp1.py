b00b82e0967160124a96d0ac184bde69b6c706cc
bhagavan <bhagavan@bhagavan>
2017-11-08 11:43:30 +0530

    adding

--- ramu/dhcp1.py
+++ ramu/dhcp1.py
@@ -1,9 +1,23 @@
-a=open("output-conf.txt","r")
-b= a.read()
-print (b)
-v=open("input-conf.txt","r")
-s =v.read()
-print (s)
-with open("write.txt","w") as w:
-		w.writelines(b)
-
+import sys 
+import csv
+with open('fixed-ip-list.csv') as fd:
+	data=[list(line) for line in csv.reader(fd)
+for row in data:
+	name=row
+	sysname=name[0]
+	macadd=name[1]
+fd.close()
+		w.write(b)
+		for i in w:
+			i.replace("santhosh-laptop","santhosh-laptop")
+			print(i)
+net = '192.168.1.215'
+host = []
+for x in range(1):
+    host.append(str(net)+str(x))
+print(host)
+'''
+net = "192.168.1.214"
+host= [net + str(i) for i in range (2)]
+print (host)
+'''
