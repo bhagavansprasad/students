2b84e3db431e549cc61882f292abcd98fcec6c37
bhagavan <bhagavan@auranetworks.in>
2020-06-02 18:49:52 +0530

    inserting an element

--- saketh/cpp/mm/arr_ins_ele_pos.cpp
+++ saketh/cpp/mm/arr_ins_ele_pos.cpp
@@ -1,42 +1,56 @@
 #include<iostream>
 #include<cstdlib>
 using namespace std;
-int insertion( int pa[10000],int val,int pla)
- {
-	int n,i=0;
-	cout<<"enter the length of the array"<<endl;
-	cin>>n;
-    cout<<"enter the elements"<<endl;
-	for(i=0;i<=n;i++)
-		cin>>pa[i];
-	cout<<"enter value to be inserted"<<endl;
-	cin>>val;
-	cout<<"enter the place the value would be inserted into"<<endl;
-	cin>>pla;
-    cout<<"initial array is"<<endl;
-	for(i=0;i<=n;i++)
-		cout<<","<<pa[i];
-	cout<<endl;
-	for(i=n-1;i>=pla;i--)
-		pa[i+1]=pa[i];
-	pa[pla]=val;
-    cout<<endl;  
-    cout<<endl;  
-    cout<<endl; 
-    
-	cout<<"after insertion is:";
-	for(i=0;i<=n;i++)
-		cout<<","<<pa[i];
-  return 0;  
- }
- int main()
- {
-	 int a[5],value=9,place=1;
-	 int insertion(a,value,place);
- }	 
+//using boost::format;
 
+int A[100] = {1,2,3,4,5};
+int noe=5;
+int dump_array(int *p, int n)// it is there just to show positions
+{
+	int i = 0;
+	cout << "Position#";
+	for (i = 0; i <= n; i++)
+	 	cout << i<<",";
+	cout << endl; 
+	cout << "elements#";
+	for (i = 0; i < n; i++)
+		cout << p[i] << ",";
+	cout << endl;
+	cout << endl;
+	return 0;
+}
 
-	
+int insertion_value_by_postion(int *pA, int val, int pos )
+{
+	int i=0;
+	cout << "Inserting " << val << " at " << pos << endl << endl;
+	if(noe == 0)
+	{
+	 	cout << "NOTHING TO INSERT THE VALUE TO "<<endl;
+		return 0;
+	}
+	if(pos > noe)
+ 	{
+	 	cout << "Invalid position :" << pos << endl;
+		return 0;
+	}	 
+	for(i = noe-1; i >= pos; i--)
+		pA[i+1] = pA[i];
+	pA[pos]=val;
+	noe++; 
+	return 0;
+}
 
-	
+int main()
+{ 
+	int value=9, pos=2;
+	dump_array(A, noe);
 
+    insertion_value_by_postion(A,  value, pos);
+	dump_array(A, noe);
+
+    insertion_value_by_postion(A, 10, 3);
+	dump_array(A, noe);
+    insertion_value_by_postion(A, 17, 0);
+	dump_array(A, noe);
+}	
