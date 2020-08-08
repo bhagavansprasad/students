44197249ad59f5fa41f3916c92e63d77022cbcbe
bhagavan <bhagavan@auranetworks.in>
2020-08-08 10:53:24 +0530

    Fixing issues

--- vachan/magic-squre.py
+++ vachan/magic-squre.py
@@ -1,20 +1,15 @@
-n=5
+n=9
 a = [[0 for i in range(n)] for j in range(n)]
-r = 0
-c = int(n/2)
 
 for i in range(n):
     for j in range(n):
         print(f"%2d" % (a[i][j]), end=" ")
     print()
 
-i=1
-
-print()
-
-
+r = 0
+c = int(n/2)
 for i in range (1,n*n+1):
-    #print(f"r :{r}, c:{c}")
+    print(f"r :{r}, c:{c}, i :{i}")
     a[r][c]=i
 
     r = r - 1
@@ -30,7 +25,7 @@ for i in range (1,n*n+1):
     if (c > n-1):
         c = 0
 
-    if(a[r][c]!=0):
+    if(a[r][c] != 0):
         r=r+2
         c=c-1
 
