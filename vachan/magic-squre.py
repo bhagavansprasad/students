508261a2a808a31446dec140039c57a676e997e2
bhagavan <bhagavan@auranetworks.in>
2020-08-08 10:47:30 +0530

    Adding magic square

--- vachan/magic-squre.py
+++ vachan/magic-squre.py
@@ -1,13 +1,7 @@
-a=[
-        [0,0,0,0,0], 
-        [0,0,0,0,0],
-        [0,0,0,0,0],
-        [0,0,0,0,0],
-        [0,0,0,0,0]
-  ]
 n=5
-r=0
-c=int(n/2)
+a = [[0 for i in range(n)] for j in range(n)]
+r = 0
+c = int(n/2)
 
 for i in range(n):
     for j in range(n):
@@ -18,27 +12,27 @@ i=1
 
 print()
 
-while(i <= n*n):
+
+for i in range (1,n*n+1):
     #print(f"r :{r}, c:{c}")
     a[r][c]=i
 
     r = r - 1
     c = c + 1
 
-    if(c==n and r<0):
+    if (r < 0 and c > n-1):
         r=r+2
         c=c-1
 
-    if(r<0):
+    if (r < 0):
         r=n-1
 
-    if(c>=n):
-        c=0
-        
+    if (c > n-1):
+        c = 0
+
     if(a[r][c]!=0):
         r=r+2
         c=c-1
-    i=i+1
 
 for i in range (n):
     for j in range(n):
